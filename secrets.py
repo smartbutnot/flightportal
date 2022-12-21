d794df5054a316ecf641ff7620553ff8bdd39d1e
smartbutnot <103124527+smartbutnot@users.noreply.github.com>
2022-12-21 10:25:10 +0000

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -4,5 +4,7 @@
 secrets = {
     'ssid' : 'wifi network',
     'password' : 'wifi password',
+    # area to search for flights: top latitude, bottom latitude, left longitude, right longitude
+    # (so this example is central London)
     'bounds_box' : '51.6,51.4,-0.3,-0.1'
     }
