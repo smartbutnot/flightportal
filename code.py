7e0381cdf868711250dde032bf247cb53c3fb594
smartbutnot <103124527+smartbutnot@users.noreply.github.com>
2023-10-22 11:33:03 +0100

    Switch search server from data-live to data-cloud.flightradar24.com

--- code.py
+++ code.py
@@ -53,7 +53,7 @@ PLANE_SPEED=0.04
 TEXT_SPEED=0.04
 
 #URLs
-FLIGHT_SEARCH_HEAD="https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds="
+FLIGHT_SEARCH_HEAD="https://data-cloud.flightradar24.com/zones/fcgi/feed.js?bounds="
 FLIGHT_SEARCH_TAIL="&faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=0&air=1&vehicles=0&estimated=0&maxage=14400&gliders=0&stats=0&ems=1&limit=1"
 FLIGHT_SEARCH_URL=FLIGHT_SEARCH_HEAD+BOUNDS_BOX+FLIGHT_SEARCH_TAIL
 # Deprecated URL used to return less JSON than the long details URL, but can give ambiguous results
