2789dfab23398463961eeee29c5f221afd9a6d03
Solomon Real <39798059+solomonreal@users.noreply.github.com>
2023-12-28 11:12:45 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,8 @@
 import time
-from random import randrange
 import board
 import terminalio
-from adafruit_matrixportal.matrixportal import MatrixPortal
-from adafruit_portalbase.network import HttpError
+from adafruit_esp32s3 import ESP32S3SPI
+from adafruit_esp32s3.wifimanager import ESP32S3SPI_WiFiManager
 import adafruit_requests as requests
 import json
 
@@ -69,15 +68,13 @@ rheaders = {
      "accept": "application/json"
 }
 
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp32_cs = DigitalInOut(board.ESP_CS)
 esp32_ready = DigitalInOut(board.ESP_BUSY)
 esp32_reset = DigitalInOut(board.ESP_RESET)
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
-status_light = neopixel.NeoPixel(
-    board.NEOPIXEL, 1, brightness=0.2
-)
-wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light,debug=False,attempts=1)
+esp32 = ESP32S3SPI(spi, esp32_cs, esp32_ready, esp32_reset)
+status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+wifi = ESP32S3SPI_WiFiManager(esp32, secrets, status_light)
 
 # Top level matrixportal object
 matrixportal = MatrixPortal(
